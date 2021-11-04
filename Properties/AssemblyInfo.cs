a673b9acbdf2faccab90f66aa3d783040c3550fc
Devil <devil0153@163.com>
2021-11-04 06:25:35 +0000

    v1.0.0

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Virtual Player Example")]
+[assembly: AssemblyDescription("Virtual player example that supports playing local and network resources (*.wmv,*.avi,*.asf,*.mov,*.mp4,*.3gp,*.mkv,*.mpeg,*.mpg...)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Demonicsoft")]
+[assembly: AssemblyProduct("VirtualPlayer")]
+[assembly: AssemblyCopyright("Copyright © 2021 Devil")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba5636f0-d235-4694-9a83-157270ccf190")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VideoExtension.cs
@@ -0,0 +1,40 @@
+﻿using Ghost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VirtualPlayer
+{
+    public class VideoExtension : IVirtualViewExtension
+    {
+        public HubBase Hub { get; private set; }
+
+        public void Dispose()
+        {
+
+        }
+
+        public FrameworkElement GetView()
+        {
+            return ((VideoHub)this.Hub).Media;
+        }
+
+        public void Initialize(IExtensionConfig config)
+        {
+            this.Hub = new VideoHub();
+        }
+
+        public void OnConnected()
+        {
+
+        }
+
+        public void OnDisconnect()
+        {
+
+        }
+    }
+}
